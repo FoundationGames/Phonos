bfd8c91a20e3330b254ef6cca09531fd08f767fc
coolsa <me@cloudchagnon.xyz>
2021-10-24 17:40:27 -0600

    Reverted Audio Position Behavior to original, but with Entity Support.

--- src/main/java/io/github/foundationgames/phonos/sound/MultiPositionedSoundInstance.java
+++ src/main/java/io/github/foundationgames/phonos/sound/MultiPositionedSoundInstance.java
@@ -2,32 +2,25 @@ package io.github.foundationgames.phonos.sound;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.AbstractSoundInstance;
-import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.TickableSoundInstance;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
-import io.github.foundationgames.phonos.Phonos;
-import io.github.foundationgames.phonos.client.ClientRecieverStorage;
-
 public class MultiPositionedSoundInstance extends AbstractSoundInstance implements TickableSoundInstance {
-    // sets of the end result type, so we don't need to iterate through all
-    // entities, and also because sticking with BlockPos's is nice...
     private final Set<BlockPos> blocks;
     private final Set<Entity> entities;
-    private final Entity player;
-
+    private double x;
+    private double y;
+    private double z;
+    
     private boolean done;
-
-    private float maxVol;
 
     public MultiPositionedSoundInstance(Set<BlockPos> blocks, Set<Entity> entities, SoundEvent sound, float volume, float pitch) {
         this(blocks, entities, sound.getId(), volume, pitch);
@@ -35,93 +28,79 @@ public class MultiPositionedSoundInstance extends AbstractSoundInstance implemen
 
     public MultiPositionedSoundInstance(Set<BlockPos> blocks, Set<Entity> entities, Identifier sound, float volume, float pitch) {
         super(sound, SoundCategory.RECORDS);
+        this.volume = volume;
+        this.pitch = pitch;
         this.blocks = blocks;
         this.entities = entities;
-        this.maxVol = volume;
-        this.pitch = pitch;
-        this.volume = 0;
-        this.player = MinecraftClient.getInstance().player;
-        // no attenuation, we are controlling this entirely.
-        this.attenuationType = SoundInstance.AttenuationType.NONE;
-        this.updatePosition();
+        updatePosition();
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public double getZ() {
+        return z;
     }
 
     private void updatePosition() {
-        if((this.entities == null || this.entities.size() == 0) && (this.blocks == null || this.blocks.size() == 0)) {
-            // if the lists are empty or don't exist, dont run the code!
-            this.volume = 0;
-            return;
-        }
-        if(this.entities.contains(this.player) && this.player.isAlive()) {
-            // if the player listening is producing audio, only play them from their POV.
-            this.x = this.player.getX();
-            this.y = -10000;
-            this.z = this.player.getZ();
-            this.volume = this.maxVol;
-            return;
-        }
-        // Assume that there is no closest block position (volume)
-        double closestDist = Double.MAX_VALUE;
+    	MinecraftClient mc = MinecraftClient.getInstance();
+        PlayerEntity player = mc.player;
 
-        // The product of all of the weights (where one weight is (dist/64)^2)
-        double totalWeight = 0.0;
-        Vec3d dirWeight = new Vec3d(0, 0, 0);
-        // The total amount of positions in the weight.
-        for(BlockPos pos : this.blocks) {
-            // for every block, lets get the square distance from that to the player.
-            double dist = pos.getSquaredDistance(this.player.getEyePos(), true);
-            // when within a 64 block radius in 3d space...
-            if(dist <= 64 * 64) {
-                // Lets convert the working position to a vector to make things a bit easier.
-                Vec3d workingPos = new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
-                // This will be the weight of a given source.
-                double tempWeight = 64 * 64 / dist - 1;
-                // If that weight is bigger than one, we can cube it
-                if(tempWeight > 1) tempWeight *= tempWeight * tempWeight;
-                // add the final weight to the total weight
-                totalWeight += tempWeight;
-                // and also add add the weighted vector to the total vector.
-                dirWeight = dirWeight.add(workingPos.multiply(tempWeight));
-                // and also, if we are now closer to a source than previously, lets set it as such.
-                if(closestDist > dist) closestDist = dist;
-            }
-        }
-        // Now, iterate through the entities too.
-        for(Entity ent : this.entities) {
-            if(ent == null || !ent.isAlive()) continue; // if the entity is dead, don't play it!
-            // for every block, lets get the current position of it as a vector.
-            Vec3d workingPos = ent.getPos();
-            // And the square distance from that to the player.
-            double dist = workingPos.squaredDistanceTo(this.player.getEyePos());
-            // when within a 64 block radius in 3d space...
-            if(dist <= 64 * 64) {
-                // This will be the weight of a given source.
-                double tempWeight = 64 * 64 / dist - 1;
-                // If that weight is bigger than one, we can cube it
-                if(tempWeight > 1) tempWeight *= tempWeight * tempWeight;
-                // add the final weight to the total weight
-                totalWeight += tempWeight;
-                // and also add add the weighted vector to the total vector.
-                dirWeight = dirWeight.add(workingPos.multiply(tempWeight));
-                // and also, if we are now closer to a source than previously, lets set it as such.
-                if(closestDist > dist) closestDist = dist;
-            }
-        }
-        if(totalWeight == 0) {
-            this.volume = 0;
+        if(blocks == null) {
+            setAsFarPositions(player);
             return;
         }
-        dirWeight = dirWeight.multiply(1.0 / totalWeight);
-        Vec3d result = dirWeight;
-        // and center the block there.
-        this.x = result.x;
-        this.y = result.y;
-        this.z = result.z;
-        // also, if the closest distance is in range,
-        if(closestDist < 64 * 64) {
-            // volume is logarithmic, right? so this makes SENSE.
-            this.volume = (float)((64*64/closestDist)-1)/64 * maxVol;
-        } else this.volume = 0.0f;
+        boolean near = false;
+        double bd = -1;
+        Vec3d bp = null;
+        for(BlockPos l : blocks) {
+            double d = l.getSquaredDistance(player.getEyePos(), false);
+            if(d < 950*(volume/2)) {
+                if(bd < 0 || d < bd) {
+                    bd = d;
+                    bp = new Vec3d(l.getX()+.5, l.getY()+.5, l.getZ()+.5);
+                }
+            }
+            if(l.isWithinDistance(player.getEyePos(), 15*(volume/2))) near = true;
+        }
+        for(Entity e : entities) {
+            double d = e.squaredDistanceTo(player.getEyePos());
+            if(d < 950*(volume/2)) {
+                if(bd < 0 || d < bd) {
+                    bd = d;
+                    bp = e.getEyePos();
+                }
+            }
+            if(d < (15*(volume/2)*15*(volume/2))) near = true;
+        }
+        if(near) setAsPlayerPositions(player);
+        else if(bp != null) {
+            this.x = bp.getX();
+            this.y = bp.getY();
+            this.z = bp.getZ();
+        }
+        else setAsFarPositions(player);
+        
+    }
+
+    private void setAsFarPositions(PlayerEntity player) {
+        this.x = player.getX();
+        this.y = player.getEyeY()+256;
+        this.z = player.getZ();
+    }
+
+    private void setAsPlayerPositions(PlayerEntity player) {
+        this.x = player.getX();
+        this.y = player.getEyeY()+1;
+        this.z = player.getZ();
     }
 
     @Override
